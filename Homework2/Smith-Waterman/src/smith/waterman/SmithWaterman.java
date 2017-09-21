a8a987983d14860de5563b0531046b60f1009c5a
John Trapp <johneyt54@gmail.com>
2017-09-21 08:37:20 -0600

    SmithWaterman class creation
    Now with proper data structures!

--- Homework2/Smith-Waterman/src/smith/waterman/SmithWaterman.java
+++ Homework2/Smith-Waterman/src/smith/waterman/SmithWaterman.java
@@ -1,14 +1,66 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Created by Samuel Shissler, John Trapp, and Brendan Tracey for
+ * Montana State University's class on Computational Biology (CSCI 451)
+ * For the fall 2017 semester.
+ * As this is a undergrad course, usage elsware is ill-advised.
  */
 package smith.waterman;
 
 /**
  *
- * @author johne
+ * @author John Trapp, Brendan Tracey, and Samuel Shissler
  */
 public class SmithWaterman {
-    
+
+    private String s, t;
+    private int score;
+
+    public SmithWaterman() {
+
+    }
+
+    public SmithWaterman(String sIn, String tIn) {
+        s = sIn;
+        t = tIn;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public String getT() {
+        return t;
+    }
+
+    public void setS(String sIn) {
+        s = sIn;
+    }
+
+    public void setT(String tIn) {
+        t = tIn;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    private void setScore(int scoreIn) {
+        score = scoreIn;
+    }
+
+    public void sort() {
+        //Start the sort
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    //The actual sort methods
+    ////////////////////////////////////////////////////////////////////////////
+    @Override
+    public String toString() {
+        String output = "";
+        //Add alignment
+        output += getScore();
+
+        return output;
+    }
 }
